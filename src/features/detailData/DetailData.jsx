b8a4cb44292141135074812d1baa473e4cd39268
krieg1234 <olegkrieg@gmail.com>
2021-02-20 19:06:37 +0300

    deploy

--- src/features/detailData/DetailData.jsx
+++ src/features/detailData/DetailData.jsx
@@ -18,13 +18,13 @@ export function DetailData(props){
     <div className='data-details' style={{textAlign:'left'}}>
       <Row>
         <Col md={6}>
-          <p>Выбран пользователь: <b>{`${firstName} ${lastName}`}</b></p>
-          <p>Описание:</p>
-          <textarea rows={5} style={{width:'100%'}} value={description?description:'Нет данных'} onChange={()=>(null)} />
-          <p>Адрес проживания: <b>{address?address.streetAddress:'Нет данных'}</b></p>
-          <p>Город: <b>{address?address.city:'Нет данных'}</b></p>
-          <p>Провинция/штат: <b>{address?address.state:'Нет данных'}</b></p>
-          <p>Индекс: <b>{address?address.zip:'Нет данных'}</b></p>
+          <p>Выбран пользователь: <b id='detail-name'>{`${firstName} ${lastName}`}</b></p>
+          <p >Описание:</p>
+          <textarea id='detail-description' rows={5} style={{width:'100%'}} value={description?description:'Нет данных'} onChange={()=>(null)} />
+          <p>Адрес проживания: <b id='detail-streetAddress'>{address?address.streetAddress:'Нет данных'}</b></p>
+          <p>Город: <b id='detail-city'>{address?address.city:'Нет данных'}</b></p>
+          <p>Провинция/штат: <b id='detail-state'>{address?address.state:'Нет данных'}</b></p>
+          <p>Индекс: <b id='detail-zip'>{address?address.zip:'Нет данных'}</b></p>
         </Col>  
       </Row>      
       
